fc60f55e5de70a6bf1f49bdb57afb2e6d116c4b5
rojani-shrestha <rojanishrestha45@gmail.com>
2026-04-10 08:35:38 +0545

    Register page created

---
+++ lib/model/register.dart
@@ -0,0 +1,26 @@
+class Auth {
+  String? name;
+  String? email;
+  String? whatsapp;
+  String? password;
+  String? countryCode;
+
+  Auth({this.name, this.email, this.whatsapp, this.password, this.countryCode});
+
+  Auth.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    email = json['email'];
+    whatsapp = json['whatsapp'];
+    password = json['password'];
+    countryCode = json['country_code'];
+  }
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['email'] = this.email;
+    data['whatsapp'] = this.whatsapp;
+    data['password'] = this.password;
+    data['country_code'] = this.countryCode;
+    return data;
+  }
+}
